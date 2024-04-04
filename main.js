083a50b3d66e31e9264c2c7a1c7cc760cd201b39
andysantos772 <andysantos436@gmail.com>
2024-04-04 10:33:03 -0700

    Add random color and increase opacity on div hover

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function createGrid(size) {
     }
     const grid = document.querySelector('.container');
     const square_size = Math.floor(grid.clientHeight / size);
-    console.log(square_size);
     for (let x = 0; x < size; x++) {
         const row = document.createElement("div");
         for (let y = 0; y < size; y++) {
@@ -12,10 +11,25 @@ function createGrid(size) {
             col.className = "square";
             col.style.minHeight = `${square_size}px`;
             col.style.minWidth = `${square_size}px`;
+            col.addEventListener("mouseover", () => addColor(col));
             row.appendChild(col);
         }
         grid.appendChild(row);
     }
+}
+
+function getRandomColor() {
+    const red = Math.random() * (256 - 0);
+    const green = Math.random() * (256 - 0);
+    const blue = Math.random() * (256 - 0);
+    return `rgb(${red},${green},${blue})`
+  }
+
+function addColor(square) {
+    if (!square.style.backgroundColor) {
+        square.style.backgroundColor = getRandomColor();
+    }
+    square.style.opacity = Number(square.style.opacity) + 0.1;
 }
 
 // Create a 16x16 grid when the page loads
