c179eb542bcbceac5664acf480290ff17c1ba6f3
andysantos772 <andysantos436@gmail.com>
2024-04-04 11:10:31 -0700

    Add clear and resize buttons for the grid

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 function createGrid(size) {
-    if (size < 16 || size > 100) {
-        return ;
-    }
     const grid = document.querySelector('.container');
+    grid.innerHTML = "";
     const square_size = Math.floor(grid.clientHeight / size);
     for (let x = 0; x < size; x++) {
         const row = document.createElement("div");
@@ -32,5 +30,28 @@ function addColor(square) {
     square.style.opacity = Number(square.style.opacity) + 0.1;
 }
 
+function clearGrid() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.style.backgroundColor = "";
+        square.style.opacity = 0;
+    })
+}
+
+function promptResize() {
+    const numberPattern = /^[0-9]+$/;
+    let resize = prompt("Enter a whole number between 16 - 100", 16);
+    while (!numberPattern.test(resize) || resize < 16 || resize > 100) {
+        resize = prompt("Invalid value. Enter a whole number between 16 - 100", 16);
+    }
+    createGrid(resize);
+}
+
 // Create a 16x16 grid when the page loads
 createGrid(16)
+
+// Buttons
+const clear = document.querySelector('#clear');
+const resize = document.querySelector('#resize');
+clear.addEventListener("click", clearGrid);
+resize.addEventListener("click", promptResize);
